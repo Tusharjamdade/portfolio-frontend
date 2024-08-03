e20df14ca65297ca5c71db8554bd1d2db52c7f04
Tusharjamdade <tusharnjamdade@gmail.com>
2024-08-03 23:14:12 +0530

    Removed comment

--- components/CommentSection.tsx
+++ components/CommentSection.tsx
@@ -85,11 +85,11 @@ export default function CommentSection() {
         >
           Post comment
         </button>
-        <button onClick={toggleDropdown} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">{isDropdownOpen ? "Hide Comments" : "Show Comments"} <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+        {/* <button onClick={toggleDropdown} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">{isDropdownOpen ? "Hide Comments" : "Show Comments"} <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
       </svg>
           
-        </button>
+        </button> */}
 
         {isModalOpen && (
           <div
