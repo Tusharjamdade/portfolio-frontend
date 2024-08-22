a1ca12790e471ff9094551296dabbc4935befd64
Tusharjamdade <tusharnjamdade@gmail.com>
2024-08-22 14:08:45 +0530

    Education Updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,5 @@
 import type { Config } from "tailwindcss"
-const {
-  default: flattenColorPalette,
-} = require("tailwindcss/lib/util/flattenColorPalette");
+
 const config = {
   darkMode: ["class"],
   content: [
